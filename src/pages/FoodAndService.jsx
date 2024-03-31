fc848e7a82a47b69c8f53ccb46956cae02da8e68
ngtueminh <nguyentueminh0305@gmail.com>
2024-03-31 18:32:42 +0700

    Add FSheader SContent FoodContent Component

--- src/pages/FoodAndService.jsx
+++ src/pages/FoodAndService.jsx
@@ -1,4 +1,13 @@
+import FSheader_Content from "../components/Foodandservice/FSheader";
+import FContent from "../components/Foodandservice/FContent";
+import SContent from "../components/Foodandservice/SContent";
 const FoodAndService = () => {
-  return <div>FoodAndService</div>;
+  return (
+  <div>
+    <FSheader_Content></FSheader_Content>
+    {/* <FContent></FContent> */}
+    <SContent></SContent>
+  </div>
+  );
 };
 export default FoodAndService;
