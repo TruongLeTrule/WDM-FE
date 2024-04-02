4828b788d357547d0460fc618422397f2e6a3eeb
Quy <frwk.HoangQuy@gmail.com>
2024-04-02 16:24:10 +0700

    Checkbox for Permission Table

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const TableFromArray2D = ({ className, data }) => {
+const TableFromArray2D = ({ className, data, action }) => {
   return (
     <table className={className}>
       <thead>
@@ -13,9 +14,17 @@ const TableFromArray2D = ({ className, data }) => {
       <tbody>
         {data.slice(1).map((row, rowIndex) => (
           <tr key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <td key={cellIndex}>{cell}</td>
-            ))}
+            {row.map((cell, cellIndex) => {
+              if (cellIndex === 0) return <td key={cellIndex}>{cell}</td>
+              return <td key={cellIndex}>
+                <Checkbox
+                  onChange={() => action(rowIndex, cellIndex)}
+                  type="checkbox"
+                  id="myCheckbox"
+                  checked={cell} />
+              </td>
+            }
+            )}
           </tr>
         ))}
       </tbody>
@@ -24,20 +33,26 @@ const TableFromArray2D = ({ className, data }) => {
 };
 
 const User = () => {
-  const PermissionAccount = [
+  const [permissionAccount, setPermissionAccount] = useState([
     ["Account Groups", "Lobby", "Order", "Food&Service", "Report", "User"],
-    ["Super Admin", 1, 1, 1, 1, 1],
-    ["Manager", 0, 0, 0, 0, 1],
-    ["Staff", 1, 1, 1, 0, 0]
-  ];
+    ["Super Admin", true, true, true, true, true],
+    ["Manager", false, false, false, false, true],
+    ["Staff", true, true, true, false, false]
+  ]);
+
+
+  const updatePermission = (rowIndex, cellIndex) => {
+    const newUpdatePermission = [...permissionAccount];
+    newUpdatePermission[rowIndex + 1][cellIndex] = !permissionAccount[rowIndex + 1][cellIndex];
+    setPermissionAccount(newUpdatePermission);
+  }
 
   return (
     <UserBlock>
-      <h4 className="blockTitle" onClick={() => console.log(PermissionAccount)}>
+      <h4 className="blockTitle" onClick={() => console.log(permissionAccount)}>
         Permissions of account groups
       </h4>
-      <StyledTableFromArray2D data={PermissionAccount} />
-
+      <StyledTableFromArray2D data={permissionAccount} action={updatePermission} />
     </UserBlock>
   );
 };
@@ -76,5 +91,15 @@ const StyledTableFromArray2D = styled(TableFromArray2D)`
     font-size: 1.25em;
   }
 `;
+
+const Checkbox = styled.input`
+  outline: none;
+  width: 25px;
+  height: 25px;
+  color: red;
+  &:hover{
+    cursor: pointer;
+  }
+`
 
 
