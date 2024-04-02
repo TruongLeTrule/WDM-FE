e45b202e689d0d1c9814330e8ed05ec6d566d48e
Quy <frwk.HoangQuy@gmail.com>
2024-04-02 18:09:26 +0700

    User Display Complete

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { Icon } from "../assets/icon";
+import SearchBox from "../components/SearchBox";
+
+const Checkbox = styled.input`
+  outline: none;
+  width: 25px;
+  height: 25px;
+  color: red;
+  &:hover{
+    cursor: pointer;
+  }
+`
 
 const PermissonAccountTable = ({ className, data, action }) => {
   return (
@@ -24,8 +35,7 @@ const PermissonAccountTable = ({ className, data, action }) => {
                   id="myCheckbox"
                   checked={cell} />
               </td>
-            }
-            )}
+            })}
           </tr>
         ))}
       </tbody>
@@ -33,7 +43,7 @@ const PermissonAccountTable = ({ className, data, action }) => {
   );
 };
 
-const AccountInformationTable = ({ className, data, action }) => {
+const AccountInformationTable = ({ className, data }) => {
   return (
     <table className={className}>
       <thead>
@@ -41,15 +51,16 @@ const AccountInformationTable = ({ className, data, action }) => {
           {data[0].map((header, index) => (
             <th key={index}>{header}</th>
           ))}
+          <th></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         {data.slice(1).map((row, rowIndex) => (
           <tr key={rowIndex}>
-            {row.map((cell, cellIndex) => {
-              return <td key={cellIndex}>{cell}</td>
-            }
-            )}
+            {row.map((cell, cellIndex) => (
+              <td key={cellIndex}>{cell}</td>
+            ))}
             <td className="pencilIcon"><Icon.pencil /></td>
             <td className="deleteIcon"><Icon.delete /></td>
           </tr>
@@ -74,6 +85,8 @@ const User = () => {
     ["QT003", "Nguyen Tue Minh", "minhtue", "Manager"],
     ["QT004", "Le Quang Truong", "truongle", "Staff"],
     ["QT005", "Le Tan Hoa", "hoale", "Staff"],
+    ["QT005", "Le Tan Hoa", "hoale", "Staff"],
+    ["QT005", "Le Tan Hoa", "hoale", "Staff"],
   ]);
 
   const updatePermission = (rowIndex, cellIndex) => {
@@ -82,18 +95,28 @@ const User = () => {
     setPermissionAccount(newUpdatePermission);
   }
 
+  const reloadPage = () => {
+    alert("Saved !!!");
+    location.reload();
+  }
+
   return (
     <UserBlock>
       <h4 className="blockTitle" onClick={() => console.log(permissionAccount)}>
         Permissions of account groups
       </h4>
       <StyledPermissionAccountTable data={permissionAccount} action={updatePermission} />
-      <h4 className="blockTitle" onClick={() => console.log(permissionAccount)}>
-        Account Information
-      </h4>
-      <StyledAccountInformationTable data={accountInformation}>
-
-      </StyledAccountInformationTable>
+      <div className="TitleSearchCombination">
+        <h4 className="blockTitle" onClick={() => console.log(permissionAccount)}>
+          Account Information
+        </h4>
+        <SearchBox />
+      </div>
+      <StyledAccountInformationTable data={accountInformation} />
+      <CreateSaveCombination>
+        <button className="createButton">Create</button>
+        <button className="saveButton" onClick={reloadPage}>Save</button>
+      </CreateSaveCombination>
     </UserBlock>
   );
 };
@@ -104,13 +127,22 @@ const UserBlock = styled.div`
   background-color: white;
   padding-left: 2%;
   padding-top: 1%;
+  width: 100%;
   height: 100vh;
+  overflow-y: scroll;
   .blockTitle {
     width: 100%;
     height: 8vh;
     font-weight: 600;
     display: flex;
     align-items: center;
+  }
+  .TitleSearchCombination{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-right: 5%;
   }
 `;
 
@@ -134,16 +166,6 @@ const StyledPermissionAccountTable = styled(PermissonAccountTable)`
     font-size: 1.25em;
   }
 `;
-
-const Checkbox = styled.input`
-  outline: none;
-  width: 25px;
-  height: 25px;
-  color: red;
-  &:hover{
-    cursor: pointer;
-  }
-`
 
 const StyledAccountInformationTable = styled(AccountInformationTable)`
   width: 100%;
@@ -175,3 +197,28 @@ const StyledAccountInformationTable = styled(AccountInformationTable)`
     }
   }
 `;
+
+const CreateSaveCombination = styled.div`
+  height: 9vh;
+  display: flex;
+  padding-left: 5%;
+  padding-right: 5%;
+  justify-content: space-between;
+  align-items: center;
+  button{
+    width: 10%;
+    height: 50%;
+    border-radius: 15px;
+    color: white;
+    font-size: 15px;
+    &:hover{
+      cursor: pointer;
+    }
+  }
+  .createButton{
+    background-color: gray;
+  }
+  .saveButton{
+    background-color: blue;
+  }
+`
