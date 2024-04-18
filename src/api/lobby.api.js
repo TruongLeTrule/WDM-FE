b38e546ad17b6091e6c91574e85f7e31314b536d
canh <pcanhgm@gmail.com>
2024-04-18 15:43:43 +0700

    chore(lobbies): add params for api

--- src/api/lobby.api.js
+++ src/api/lobby.api.js
@@ -28,12 +28,25 @@ export const deleteLobType = (id) => {
 =================== LOBBY ===================
 */
 
-export const getLobbies = () => {
-  return axiosClient.get('lobby');
+export const getLobbies = ( date, lob_type_id ) => {
+  const params = {}
+  if(date) {
+    params.date = params
+  }
+  if(lob_type_id) {
+    params.lob_type_id = lob_type_id
+  }
+
+
+  return axiosClient.get('lobby', {
+    params
+  });
 };
 
-export const getLobbyById = (id) => {
-  return axiosClient.get(`lobby/${id}`);
+export const getLobbyById = (id, includeDeleted=false) => {
+  return axiosClient.get(`lobby/${id}`, {
+    params: { includeDeleted, },
+  });
 };
 
 export const createLobby = (dataCreate) => {
